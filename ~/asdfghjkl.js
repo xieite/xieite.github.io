9e23137a0e63cafe5b7b20a44910d8f5f3c28b75
Eczbek <eczbek.void@gmail.com>
2025-07-30 11:57:41 -0400

    Improve links

--- ~/asdfghjkl.js
+++ ~/asdfghjkl.js
@@ -1,2 +1,2 @@
 const f = (a, b) => a.split`
-`.filter((x,i)=>!(i%4)).map(x=>x.split('XIEITE_'+b+'_TYPE_')[1].split(' ')[0]).map(x=>`<a href="https://github.com/search?q=repo:Eczbek/xieite path:include/xieite/pp/${b.toLowerCase()}.hpp /XIEITE_${b}_TYPE_${x}$/&type=code" target="_blank"><code><i_>${x}</i_></code></a>`).join`, `
+`.filter((x,i)=>!(i%4)).map(x=>x.split('XIEITE_'+b+'_TYPE_')[1].split(' ')[0]).map(x=>`<a target="_blank" href="https://github.com/search?q=repo:Eczbek/xieite path:include/xieite/pp/${b.toLowerCase()}.hpp /XIEITE_${b}_TYPE_${x}$/&type=code"><code><i_>${x}</i_></code></a>`).join`, `
