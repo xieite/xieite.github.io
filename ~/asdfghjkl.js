22b99a254cf1cc441441b101f3b2e903ffe68ed2
Eczbek <eczbek.void@gmail.com>
2026-02-13 21:30:05 -0500

    update

--- ~/asdfghjkl.js
+++
@@ -1,2 +0,0 @@
-const f = (a, b) => a.split`
-`.filter((x,i)=>!(i%4)).map(x=>x.split('XIEITE_'+b+'_TYPE_')[1].split(' ')[0]).map(x=>`<a target="_blank" href="https://github.com/search?q=repo:Eczbek/xieite path:include/xieite/pp/${b.toLowerCase()}.hpp /XIEITE_${b}_TYPE_${x}$/&type=code"><code><i_>${x}</i_></code></a>`).join`, `
