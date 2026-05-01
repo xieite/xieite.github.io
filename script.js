385f07d6b74ea0a6b75a133a34e8ab3a448c0427
Eczbek <eczbek.void@gmail.com>
2026-05-01 13:44:33 -0400

    update

--- script.js
+++
@@ -1,2 +0,0 @@
-addEventListener('load', () => document.body.scrollTop = +localStorage.getItem(location.pathname));
-addEventListener('beforeunload', () => localStorage.setItem(location.pathname, document.body.scrollTop));
